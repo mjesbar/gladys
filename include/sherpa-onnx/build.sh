41619166207249e03373989be8edb1c75bd812b5
mjesbar <miguedjba96@gmail.com>
2026-05-17 00:32:03 -0500

    fix: rm sherpaonnx include folder from track

--- include/sherpa-onnx/build.sh
+++ include/sherpa-onnx/build.sh
@@ -3,9 +3,9 @@
 PWD=$(pwd)
 
 # Activate CUDA 12.4.0 for this build
-source env.sh
+# source env.sh
 
-echo "${CUDA_HOME}"
+# echo "${CUDA_HOME}"
 
 # exit
 
@@ -28,8 +28,8 @@ cmake \
   -DCMAKE_BUILD_TYPE=Release \
   -DBUILD_SHARED_LIBS=ON \
   -DCMAKE_INSTALL_PREFIX=${PWD}/shared \
-  -DSHERPA_ONNX_ENABLE_GPU=ON \
   ..
+  # -DSHERPA_ONNX_ENABLE_GPU=ON \
 
 make -j12
 make install
